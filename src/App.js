72dfb778ec90007f2f36f16baf0807879501bce1
Henry Moland <molandh@gmail.com>
2019-05-15 07:15:01 -0700

    Created copy button. Need to style

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import seedColors from './seedColors';
 function App() {
   return (
     <div className="App">
-      <Palette palette {...seedColors[2]}/>
+      <Palette palette {...seedColors[4]}/>
     </div>
   );
 }
--- src/Palette.js
+++ src/Palette.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
+import ColorBox from './ColorBox';
+import './Palette.css';
 
 class Palette extends Component {
-    
+
     render() {
+        const ColorBoxes = this.props.colors.map(color => (
+            <ColorBox background= {color.color}  name={color.name}/>
+        ))
+
         return (
             <div className='palette'>
             {/* Navbar */}
 
                 <div className='palette-colors'>
-                {/* Color boxes */}
+                {ColorBoxes}
 
                 </div>
                 {/* Footer */}
